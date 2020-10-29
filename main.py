eef42f3cb589b7f4096fa0288ad98c41b7574dfa
Marco Magisano <mmagisano@fi.uba.ar>
2020-10-29 01:46:42 -0300

    Welcome message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ letters = ['M', 'CM','D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I']
 values = [1000, 900,  500, 400, 100,  90, 50, 40, 10, 9,  5, 4,  1]
 
 try :
-  num = int(input("Please insert a number between 1 and 3999: "))
+  num = int(input("Welcome to IntegerToRoman! Please insert a number between 1 and 3999: "))
 except :
   print("An error occured. Please insert a number between 1 and 3999")
   exit()
